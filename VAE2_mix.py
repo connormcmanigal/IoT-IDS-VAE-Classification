64cbefb9f3d6060a608302848dd443e58f029059
aamui <aaronrubenmui@gmail.com>
2024-06-13 14:37:18 -0700

    more varialbe changes

--- VAE2_mix.py
+++ VAE2_mix.py
@@ -6,11 +6,11 @@ import numpy as np
 
 
 class Encoder(nn.Module):
-    def __init__(self,input_dim, latent_dim, hidden_dim):
+    def __init__(self,input_dim, z_dim, h_dim):
         super(Encoder, self).__init__()
-        self.fc1=nn.Linear(input_dim, hidden_dim)
-        self.mu=nn.Linear(hidden_dim, latent_dim)
-        self.sigma=nn.Linear(hidden_dim, latent_dim)
+        self.fc1=nn.Linear(input_dim, h_dim)
+        self.mu=nn.Linear(h_dim, z_dim)
+        self.sigma=nn.Linear(h_dim, z_dim)
         self.relu=nn.ReLU()
     def forward(self, x):
         x=self.relu(self.fc1(x))
@@ -19,10 +19,10 @@ class Encoder(nn.Module):
         return mu,sigma
     
 class Decoder(nn.Module):
-    def __init__(self, output_dim, latent_dim, hidden_dim):
+    def __init__(self, input_dim, z_dim, h_dim):
         super(Decoder,self).__init__()
-        self.fc1=nn.Linear(latent_dim, hidden_dim)
-        self.out=nn.Linear(hidden_dim, output_dim)
+        self.fc1=nn.Linear(z_dim, h_dim)
+        self.out=nn.Linear(h_dim, input_dim)
         self.relu=nn.ReLU()
     def forward(self, z):
         z=self.relu(self.fc1(z))
@@ -30,10 +30,10 @@ class Decoder(nn.Module):
         return recon
 
 class VAE4(nn.Module):
-    def __init__(self, latent_dim, in_out_dim, hidden_dim=100):
+    def __init__(self, z_dim, input_dim, h_dim):
         super(VAE4,self).__init__()
-        self.encoder=Encoder(in_out_dim, latent_dim, hidden_dim=hidden_dim)
-        self.decoder=Decoder(in_out_dim, latent_dim, hidden_dim=hidden_dim)
+        self.encoder=Encoder(input_dim, z_dim, h_dim)
+        self.decoder=Decoder(input_dim, z_dim, h_dim)
     def forward(self, x):
         mu, sigma= self.encoder(x)
         z=self.sample(mu,sigma)
