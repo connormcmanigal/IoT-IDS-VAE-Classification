e01c3dd37ae2e0ab7fe571507d17e44d01adfeac
aamui <aaronrubenmui@gmail.com>
2024-06-13 13:17:41 -0700

    added loss into class

--- VAE2_mix.py
+++ VAE2_mix.py
@@ -45,32 +45,31 @@ class VAE4(nn.Module):
         z=mu+(sigma*norm_rand)
         return z
     
+    def combo_elbo(self,reconstructed_data, true_data, z_mu, z_sigma, cat_feature_indicies):
+        # https://pytorch.org/docs/stable/generated/torch.nn.SmoothL1Loss.html
+        # Supposedly more robust than MSE.
 
-def combo_elbo(reconstructed_data, true_data, z_mu, z_sigma, cat_feature_indicies):
-    # https://pytorch.org/docs/stable/generated/torch.nn.SmoothL1Loss.html
-    # Supposedly more robust than MSE.
+        # Splitting categorical and continous
+        cat_mask=torch.zeros(reconstructed_data.size(1), dtype=torch.bool)
+        cat_mask[cat_feature_indicies]=True
+        cont_mask=~cat_mask
 
-    # Splitting categorical and continous
-    cat_mask=torch.zeros(reconstructed_data.size(1), dtype=torch.bool)
-    cat_mask[cat_feature_indicies]=True
-    cont_mask=~cat_mask
+        recon_cat = reconstructed_data[:, cat_mask]
+        recon_cont = reconstructed_data[:, cont_mask]
+        true_cat = true_data[:, cat_mask]
+        true_cont = true_data[:, cont_mask]
 
-    recon_cat = reconstructed_data[:, cat_mask]
-    recon_cont = reconstructed_data[:, cont_mask]
-    true_cat = true_data[:, cat_mask]
-    true_cont = true_data[:, cont_mask]
+        hubert=F.smooth_l1_loss(recon_cont, true_cont, reduction='none')
+        hubert=torch.mean(torch.sum(hubert, dim=-1))
+        
+        #https://pytorch.org/docs/stable/generated/torch.nn.functional.binary_cross_entropy_with_logits.html
+        bce_logits=F.binary_cross_entropy_with_logits(recon_cat, true_cat, reduction='none')
+        bce_logits=torch.mean(torch.sum(bce_logits, dim=-1))
 
-    hubert=F.smooth_l1_loss(recon_cont, true_cont, reduction='none')
-    hubert=torch.mean(torch.sum(hubert, dim=-1))
-    
-    #https://pytorch.org/docs/stable/generated/torch.nn.functional.binary_cross_entropy_with_logits.html
-    bce_logits=F.binary_cross_entropy_with_logits(recon_cat, true_cat, reduction='none')
-    bce_logits=torch.mean(torch.sum(bce_logits, dim=-1))
+        kl_divergence = -0.5 * torch.sum(1 + 2 * torch.log(z_sigma.clamp(min=1e-8)) - z_mu**2 - z_sigma**2, -1)
+        kl_divergence = torch.mean(kl_divergence)
 
-    kl_divergence = -0.5 * torch.sum(1 + 2 * torch.log(z_sigma.clamp(min=1e-8)) - z_mu**2 - z_sigma**2, -1)
-    kl_divergence = torch.mean(kl_divergence)
+        recon_tot = hubert + bce_logits
 
-    recon_tot = hubert + bce_logits
-
-    elbo = recon_tot + kl_divergence
-    return elbo
+        elbo = recon_tot + kl_divergence
+        return elbo
